9a6256c54ee06075d8aaf7c8e28bd0485cb56623
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-12-24 21:47:02 +0530

    Add files via upload

--- fastapi-http-methods/main.py
+++ fastapi-http-methods/main.py
@@ -1,5 +1,21 @@
+import json
+
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load()
+
+    return data
+
 @app.get("/")
+def hello():
+    return {"message": "Patient Management System API"}
+
+@app.get("/about")
+def about():
+    return {"message": "Fully functional API to manage patient's records"}
+
