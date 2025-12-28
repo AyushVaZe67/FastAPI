d3e8232a550cc42d907709c8ff0a2de10f011404
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-12-28 23:33:24 +0530

    Add files via upload

--- pydantic_tutorial/1_pydantic_why.py
+++ pydantic_tutorial/1_pydantic_why.py
@@ -1,24 +1,26 @@
 from pydantic import BaseModel
+from typing import List, Dict, Optional
 
 class Patient(BaseModel):
     name: str
     age: int
+    weight: float
+    married: bool
+    allergies: Optional[List[str]] = None
+    contact_details: Dict[str, str]
 
 def insert_patient_data(patient: Patient):
     print(patient.name)
-    # print(patient.age)
     print('inserted')
 
 def update_patient_data(patient: Patient):
     print(patient.name)
-    # print(patient.age)
     print('updated')
 
 
-patient_info = {'name':'Ayush','age':22}
+patient_info = {'name':'Ayush','age':'22', 'weight':'87.3', 'married':True, 'allergies':['pollen','dust','chicken'], 'contact_details':{'email':'abc@gmail.com','phone':'9988776655'}}
 
 p1 = Patient(**patient_info)
-
 
 insert_patient_data(p1)
 update_patient_data(p1)
