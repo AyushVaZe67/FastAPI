028a3c9c4fa60ccd8ab9a24ccb8cd3c18d5ef74b
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-12-28 23:07:25 +0530

    Add files via upload

--- pydantic_tutorial/1_pydantic_why.py
+++ pydantic_tutorial/1_pydantic_why.py
@@ -1,2 +1,6 @@
-def insert_patient_data(name, age):
-    
+def insert_patient_data(name : str, age: int):
+    print(name)
+    print(age)
+    print('Inserted in DB')
+
+insert_patient_data('Ayush',11)
