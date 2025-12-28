13ffb31b833e37c0505e7bf8eb36cee63a41672b
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-12-28 23:38:36 +0530

    Add files via upload

--- pydantic_tutorial/1_pydantic_why.py
+++ pydantic_tutorial/1_pydantic_why.py
@@ -1,13 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, AnyUrl
 from typing import List, Dict, Optional
 
 class Patient(BaseModel):
     name: str
+    email: EmailStr
     age: int
     weight: float
-    married: bool
+    married: bool = False
     allergies: Optional[List[str]] = None
     contact_details: Dict[str, str]
+    linkedin_url : AnyUrl
 
 def insert_patient_data(patient: Patient):
     print(patient.name)
@@ -18,7 +20,7 @@ def update_patient_data(patient: Patient):
     print('updated')
 
 
-patient_info = {'name':'Ayush','age':'22', 'weight':'87.3', 'married':True, 'allergies':['pollen','dust','chicken'], 'contact_details':{'email':'abc@gmail.com','phone':'9988776655'}}
+patient_info = {'name':'Ayush','email':'a@a.a','linkedin_url':'http://linkedin.com/1322','age':'22', 'weight':'87.3', 'married':True, 'allergies':['pollen','dust','chicken'], 'contact_details':{'email':'abc@gmail.com','phone':'9988776655'}}
 
 p1 = Patient(**patient_info)
 
