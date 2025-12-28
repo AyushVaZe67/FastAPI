aaa49a71feed2ce6fb1e50688d20c139fd22d765
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-12-28 23:50:46 +0530

    Add files via upload

--- pydantic_tutorial/1_pydantic_why.py
+++ pydantic_tutorial/1_pydantic_why.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel, EmailStr, AnyUrl, Field
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Annotated
 
 class Patient(BaseModel):
-    name: str = Field(max_length=50)
+    name: Annotated[str, Field(max_length=50, title='Name of Patient', description='this is name of the patient', examples=['Mukesh','Ramesh'])]
     email: EmailStr
     age: int = Field(gt=0,lt=120)
-    weight: float = Field(gt=0, lt=100)
-    married: bool = False
-    allergies: Optional[List[str]] = Field(max_length=5)
+    weight: Annotated[float, Field(gt=0, lt=100, strict=True)]
+    married: Annotated[bool, Field(default=None, description='Is patient married or not')]
+    allergies: Annotated[Optional[List[str]], Field(default=None, max_length=5)]
     contact_details: Dict[str, str]
     linkedin_url : AnyUrl
 
@@ -20,7 +20,7 @@ def update_patient_data(patient: Patient):
     print('updated')
 
 
-patient_info = {'name':'Ayush','email':'a@a.a','linkedin_url':'http://linkedin.com/1322','age':'22', 'weight':'87.3', 'married':True, 'allergies':['pollen','dust','chicken'], 'contact_details':{'email':'abc@gmail.com','phone':'9988776655'}}
+patient_info = {'name':'Ayush','email':'a@a.a','linkedin_url':'http://linkedin.com/1322','age':'22', 'weight':87.3, 'married':True, 'allergies':['pollen','dust','chicken'], 'contact_details':{'email':'abc@gmail.com','phone':'9988776655'}}
 
 p1 = Patient(**patient_info)
 
