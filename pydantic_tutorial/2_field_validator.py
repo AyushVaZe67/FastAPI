bc358be9904839a9789d448a5de567621f673108
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-12-29 16:19:55 +0530

    Add files via upload

--- pydantic_tutorial/2_field_validator.py
+++ pydantic_tutorial/2_field_validator.py
@@ -1,1 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator
+from typing import List, Dict, Optional, Annotated
+
+class Patient(BaseModel):
+
+    name: str
+    email: EmailStr
+    age: int
+    weight: float
+    married: bool
+    allergies: List[str]
+    contact_details: Dict[str, str]
+
+
+def update_patient_data(patient: Patient):
+
+    print(patient.name)
+    print(patient.age)
+    print(patient.allergies)
+    print(patient.married)
+    print('updated')
