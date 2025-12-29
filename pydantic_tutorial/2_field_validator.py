d02cb814b7857b12fd378cd0de35faaa0820322a
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-12-29 16:23:51 +0530

    Add files via upload

--- pydantic_tutorial/2_field_validator.py
+++ pydantic_tutorial/2_field_validator.py
@@ -11,6 +11,18 @@ class Patient(BaseModel):
     allergies: List[str]
     contact_details: Dict[str, str]
 
+    @field_validator('email')
+    @classmethod
+    def email_validator(cls, value):
+
+        valid_domains = ['hdfc.com','icici.com']
+        domain_name = value.split('@')[-1]
+
+        if domain_name not in valid_domains:
+            raise ValueError('Not a Valid Domain!')
+
+        return value
+
 
 def update_patient_data(patient: Patient):
 
@@ -19,3 +31,10 @@ def update_patient_data(patient: Patient):
     print(patient.allergies)
     print(patient.married)
     print('updated')
+
+
+patient_info = {'name':'Ayush', 'email':'abc@icici.com', 'age': '30', 'weight': 75.2, 'married': True, 'allergies': ['pollen', 'dust'], 'contact_details':{'phone':'2353462'}}
+
+patient1 = Patient(**patient_info) # validation -> type coercion
+
+update_patient_data(patient1)
