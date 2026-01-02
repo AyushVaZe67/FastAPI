acc6e78da32b1a30061a5d2a64c291f22cba71fa
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2026-01-02 15:02:16 +0530

    Add files via upload

--- fastapi-post-request/post_request.py
+++ fastapi-post-request/post_request.py
@@ -1,6 +1,6 @@
 import json
 
-from typing import Annotated
+from typing import Annotated, Literal
 from fastapi import FastAPI, HTTPException, Path, Query
 from pydantic import BaseModel, Field
 
@@ -9,13 +9,13 @@ app = FastAPI()
 
 class Patient(BaseModel):
 
-    id: str
-    name: str
-    city: str
-    age: int
-    gender: str
-    height: float
-    weight: float
+    id: Annotated[str, Field(..., description='ID of the patient', examples=['P001'])]
+    name: Annotated[str, Field(..., description='Name of the patient')]
+    city: Annotated[str, Field(..., description='City where the patient is living')]
+    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the patient')]
+    gender: Annotated[Literal['male', 'female', 'others'], Field(..., description='Gender of the patient')]
+    height: Annotated[float, Field(..., gt=0, description='Height of the patient in mtrs')]
+    weight: Annotated[float, Field(..., gt=0, description='Weight of the patient in kgs')]
 
 def load_data():
     pass
